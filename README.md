d3c734d36bbdefb9d5ed966aecae28202c9b4946
Wasin Waeosri <wasin.waeosri@thomsonreuters.com>
2017-05-02 18:34:45 +0700

    # Article-EMA-Java-ExchangeShortName ## Overview For some instruments, suffix of RIC indicates the exchange where it is traded, e.g. HSBA.L (.L = LSE - London Stock Exchange), HBC.N (.N = NYSE - New York Stock Exchange). However, there are some instruments without the exchange code suffix like TWXF2Z7 (Time Warner DEC7), KC4F5Z7 (Kone Corp DEC7), etc. How the application know the exchange of those instruments?
    ## Solution
    The Elektron API 3.1.0 and above can retrieve a short exchange name information from the FID 1709 (Field name RDN_EXCHD2) for the Market Price domain. This FID 1709 is the enumeration field that used in the most of the exchanges.
    
    #### RDMFieldDictionary
    ```
    RDN_EXCHD2 "EXCHANGE ID 2"       1709  NULL        ENUMERATED    5 ( 3 )  ENUM             2
    ```
    
    #### enumtype.def
    ```
    RDN_EXCHD2  1709
    ...
    ! VALUE      DISPLAY   MEANING
    ! -----      -------   -------
          0        "   "   undefined
          1        "ASE"   NYSE AMEX
          2        "NYS"   New York Stock Exchange
          3        "BOS"   Boston Stock Exchange
          4        "CIN"   National Stock Exchange (formerly Cincinnati Stock Exchange)
          5        "PSE"   NYSE Arca
          6        "XPH"   NASDAQ OMX PSX when trading in SIAC (formerly Philadelphia Stock Exchange)
    	...
          1454     "PCW"   PetroChem Wire LLC
          1455     "SMP"   Euronext - Smartpool
          1456     "BT1"   BATS ONE - LEVEL 1 (PRODUCT)
    ```

---
+++ README.md
@@ -0,0 +1,30 @@
+# Article-EMA-Java-ExchangeShortName
+## Overview
+For some instruments, suffix of RIC indicates the exchange where it is traded, e.g. HSBA.L (.L = LSE - London Stock Exchange), HBC.N (.N = NYSE - New York Stock Exchange). However, there are some instruments without the exchange code suffix like TWXF2Z7 (Time Warner DEC7), KC4F5Z7 (Kone Corp DEC7), etc. How the application know the exchange of those instruments?
+
+## Solution
+The Elektron API 3.1.0 and above can retrieve a short exchange name information from the FID 1709 (Field name RDN_EXCHD2) for the Market Price domain. This FID 1709 is the enumeration field that used in the most of the exchanges.
+
+#### RDMFieldDictionary
+```
+RDN_EXCHD2 "EXCHANGE ID 2"       1709  NULL        ENUMERATED    5 ( 3 )  ENUM             2
+```
+
+#### enumtype.def
+```
+RDN_EXCHD2  1709
+...
+! VALUE      DISPLAY   MEANING
+! -----      -------   -------
+      0        "   "   undefined
+      1        "ASE"   NYSE AMEX
+      2        "NYS"   New York Stock Exchange
+      3        "BOS"   Boston Stock Exchange
+      4        "CIN"   National Stock Exchange (formerly Cincinnati Stock Exchange)
+      5        "PSE"   NYSE Arca
+      6        "XPH"   NASDAQ OMX PSX when trading in SIAC (formerly Philadelphia Stock Exchange)
+	...
+      1454     "PCW"   PetroChem Wire LLC
+      1455     "SMP"   Euronext - Smartpool
+      1456     "BT1"   BATS ONE - LEVEL 1 (PRODUCT)
+```
---
+++ src/com/thomsonreuters/platformservices/article/ExchangeName.java
@@ -0,0 +1,149 @@
+package com.thomsonreuters.platformservices.article;
+
+import com.thomsonreuters.ema.access.Msg;
+import com.thomsonreuters.ema.access.AckMsg;
+import com.thomsonreuters.ema.access.GenericMsg;
+import com.thomsonreuters.ema.access.OmmArray;
+import com.thomsonreuters.ema.access.RefreshMsg;
+import com.thomsonreuters.ema.access.StatusMsg;
+import com.thomsonreuters.ema.access.UpdateMsg;
+import com.thomsonreuters.ema.access.Data;
+import com.thomsonreuters.ema.access.DataType;
+import com.thomsonreuters.ema.access.DataType.DataTypes;
+import com.thomsonreuters.ema.access.EmaFactory;
+import com.thomsonreuters.ema.access.FieldEntry;
+import com.thomsonreuters.ema.access.FieldList;
+import com.thomsonreuters.ema.access.OmmConsumer;
+import com.thomsonreuters.ema.access.OmmConsumerClient;
+import com.thomsonreuters.ema.access.OmmConsumerEvent;
+import com.thomsonreuters.ema.access.OmmException;
+import com.thomsonreuters.ema.access.ElementList;
+import com.thomsonreuters.ema.rdm.EmaRdm;
+
+/**
+ * Created by Wasin Waeosri on 5/2/2017.
+ */
+
+class AppClient implements OmmConsumerClient {
+    //A callback function to receive REFRESH message
+    public void onRefreshMsg(RefreshMsg refreshMsg, OmmConsumerEvent event) {
+        System.out.println("Item Name: " + (refreshMsg.hasName() ? refreshMsg.name() : "<not set>"));
+        System.out.println("Service Name: " + (refreshMsg.hasServiceName() ? refreshMsg.serviceName() : "<not set>"));
+
+        System.out.println("Item State: " + refreshMsg.state());
+
+        if (DataType.DataTypes.FIELD_LIST == refreshMsg.payload().dataType())
+            decode(refreshMsg.payload().fieldList());
+
+        System.out.println();
+    }
+
+    //A callback function to receive UPDATE messages
+    public void onUpdateMsg(UpdateMsg updateMsg, OmmConsumerEvent event) {
+        System.out.println("Item Name: " + (updateMsg.hasName() ? updateMsg.name() : "<not set>"));
+        System.out.println("Service Name: " + (updateMsg.hasServiceName() ? updateMsg.serviceName() : "<not set>"));
+
+        if (DataType.DataTypes.FIELD_LIST == updateMsg.payload().dataType())
+            decode(updateMsg.payload().fieldList());
+
+        System.out.println();
+    }
+
+    //A callback function to receive STATUS messages
+    public void onStatusMsg(StatusMsg statusMsg, OmmConsumerEvent event) {
+        System.out.println("Item Name: " + (statusMsg.hasName() ? statusMsg.name() : "<not set>"));
+        System.out.println("Service Name: " + (statusMsg.hasServiceName() ? statusMsg.serviceName() : "<not set>"));
+
+        if (statusMsg.hasState())
+            System.out.println("Item State: " + statusMsg.state());
+
+        System.out.println();
+    }
+
+    public void onGenericMsg(GenericMsg genericMsg, OmmConsumerEvent consumerEvent) {
+    }
+
+    public void onAckMsg(AckMsg ackMsg, OmmConsumerEvent consumerEvent) {
+    }
+
+    public void onAllMsg(Msg msg, OmmConsumerEvent consumerEvent) {
+    }
+
+    //Decode incoming data FieldList object
+    void decode(FieldList fieldList) {
+        for (FieldEntry fieldEntry : fieldList) {
+            System.out.print("Fid: " + fieldEntry.fieldId() + " Name = " + fieldEntry.name() + " DataType: " + DataType.asString(fieldEntry.load().dataType()) + " Value: ");
+
+            if (Data.DataCode.BLANK == fieldEntry.code())
+                System.out.println(" blank");
+            else
+                switch (fieldEntry.loadType()) {
+                    case DataTypes.REAL:
+                        //System.out.println(fieldEntry.real().asDouble());
+                        System.out.println(fieldEntry.real().asDouble());
+                        break;
+                    case DataTypes.DATE:
+                        System.out.println(fieldEntry.date().day() + " / " + fieldEntry.date().month() + " / " + fieldEntry.date().year());
+                        break;
+                    case DataTypes.TIME:
+                        System.out.println(fieldEntry.time().hour() + ":" + fieldEntry.time().minute() + ":" + fieldEntry.time().second() + ":" + fieldEntry.time().millisecond());
+                        break;
+                    case DataTypes.INT:
+                        System.out.println(fieldEntry.intValue());
+                        break;
+                    case DataTypes.UINT:
+                        System.out.println(fieldEntry.uintValue());
+                        break;
+                    case DataTypes.ASCII:
+                        System.out.println(fieldEntry.ascii());
+                        break;
+                    case DataTypes.ENUM: //Parsing ENUM data from enumtype.def information
+                        System.out.println(fieldEntry.hasEnumDisplay() ? fieldEntry.enumDisplay() : fieldEntry.enumValue());
+                        break;
+                    case DataTypes.RMTES:
+                        System.out.println(fieldEntry.rmtes());
+                        break;
+                    case DataTypes.ERROR:
+                        System.out.println("(" + fieldEntry.error().errorCodeAsString() + ")");
+                        break;
+                    default:
+                        System.out.println();
+                        break;
+                }
+        }
+    }
+}
+
+public class ExchangeName {
+    public static void main(String[] args) {
+        OmmConsumer consumer = null;
+        try {
+            AppClient appClient = new AppClient();
+            //Initialize OmmConsumer object
+            consumer = EmaFactory.createOmmConsumer(EmaFactory.createOmmConsumerConfig().host("172.20.33.11:14002").username("user"));
+
+            //View
+            ElementList view = EmaFactory.createElementList();
+            OmmArray array = EmaFactory.createOmmArray();
+
+            array.fixedWidth(2);
+            //Add interested FIDs
+            array.add(EmaFactory.createOmmArrayEntry().intValue(3));
+            array.add(EmaFactory.createOmmArrayEntry().intValue(22));
+            array.add(EmaFactory.createOmmArrayEntry().intValue(25));
+            array.add(EmaFactory.createOmmArrayEntry().intValue(1709));
+
+
+            view.add(EmaFactory.createElementEntry().uintValue(EmaRdm.ENAME_VIEW_TYPE, 1));
+            view.add(EmaFactory.createElementEntry().array(EmaRdm.ENAME_VIEW_DATA, array));
+            //Send a request message to Elektron with View request in the payload
+            consumer.registerClient(EmaFactory.createReqMsg().serviceName("ELEKTRON_DD").name("HSBA.L").payload(view), appClient);
+
+            Thread.sleep(60000);            // API calls onRefreshMsg(), onUpdateMsg() and onStatusMsg()
+        } catch (InterruptedException | OmmException excp) {
+            System.out.println(excp.getMessage());
+        } finally {
+            if (consumer != null) consumer.uninitialize();
+        }
+    }
+}
